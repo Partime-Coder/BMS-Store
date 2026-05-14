4289bb780773ea0f4f4fff3b916c989bdc80301b
Sujal Bhagat <partimerbhindmask@gmail.com>
2026-05-14 13:50:59 +0530

    detailproductpage added

--- src/pages/productPages/ProductDetailPage.jsx
+++ src/pages/productPages/ProductDetailPage.jsx
@@ -12,7 +12,7 @@ function ProductDetailPage() {
   if (isError || !product) return <p className="p-4 text-red-500">Product not found.</p>
  
   return (
-    <div className=" w-full md:max-w-7xl mx-1.5 md:mx-auto py-4">
+    <div className=" w-full md:max-w-7xl px-1.5 md:mx-auto py-4">
       {/* Breadcrumb */}
       <p className="text-xs text-gray-400 mb-3">
         Home › {product.category} › {product.title}
